e5ef2d9660d027a8e37b320f03ce00659dbb75e5
ThomKiesewetterPrive <thom@kiesewetter.nl>
2015-07-25 11:17:19 +0200

    Prevent close form of dirty  ajax form

--- Sample/UserManagement/wwwroot/js/site.js
+++ Sample/UserManagement/wwwroot/js/site.js
@@ -5,10 +5,10 @@
 var mvc = { "JQuery": { "Datatables": { ajax: {}, tableTools: {}, column: {} } } };
 
 mvc.JQuery.Datatables.column.createMailToLink = function (data, type, full, meta) {
-    return '<a href=mailto:' + data + '>' + data + '</a>';
+    return "<a href=mailto:" + data + ">" + data + "</a>";
 }
 mvc.JQuery.Datatables.column.formatDate = function (data, type, full, meta) {
-    return data ? moment(data).format(meta.settings.aoColumns[meta.col].mvc6Par || "YY-DD-mm hh:mm:ss") : null;
+    return data ? window.moment(data).format(meta.settings.aoColumns[meta.col].mvc6Par || "YY-DD-mm hh:mm:ss") : null;
 }
 mvc.JQuery.Datatables.returnNull = function (data) { return null; }
 mvc.JQuery.Datatables.getLengthMenu = function () { return [[5, 50, 100, -1], [5, 50, 100, "All"]] };
@@ -16,7 +16,7 @@ mvc.JQuery.Datatables.getLanguage = function () { return {} };
 mvc.JQuery.Datatables.ajax.success = function (data, textStatus, jqXHR) { };
 mvc.JQuery.Datatables.ajax.error = function (jqXHR, textStatus, errorThrown) { alert(errorThrown) };
 mvc.JQuery.Datatables.ajax.load = function (response, textStatus, xhr) {
-    if (textStatus === 'error')
+    if (textStatus === "error")
         alert(xhr.status + " " + xhr.statusText);
 
 };
@@ -25,12 +25,12 @@ mvc.JQuery.Datatables.ajax.load = function (response, textStatus, xhr) {
 // Read all information that can be used for data request from the table and th tags
 $(document).ready(function () {
     $('table.datatables').each(function () {
-        $(this).MvcDatatable();
+        $(this).MvcDatatable({ leaveMessage: "De wijzigingen zijn niet opgeslagen! toch door gaan?" });
         //connect.datatable = give tabletool access to datatable to redraw the table after save form
     });
 });
 jQuery.fn.extend({
-    //todo dynamic button 
+    //todo muti-delete
     MvcDatatable: function (options) {
         var connect = {};
         var $datatable = $(this);
@@ -82,6 +82,7 @@ jQuery.fn.extend({
                     var id = this.fnGetSelectedData()[0].Id;
                     $("#myModal").ajaxForm({
                         url: editLink + id,
+                        leaveMessage: options.leaveMessage,
                         dataChanged: function () {
                             connect.tableTools.fnSelectNone();//Fix tabletool bug to disable buttons when no row is selected
                             connect.datatable.fnDraw(false);//redraw the table to show the new data
@@ -100,6 +101,7 @@ jQuery.fn.extend({
             fnClick: function (nButton, oConfig) {
                 $("#myModal").ajaxForm({
                     url: createLink,
+                    leaveMessage: options.leaveMessage,
                     dataChanged: function () {
                         connect.tableTools.fnSelectNone();//Fix tabletool bug to disable buttons when no row is selected
                         connect.datatable.fnDraw(false);//redraw the table to show the new data
@@ -119,13 +121,13 @@ jQuery.fn.extend({
                     var id = this.fnGetSelectedData()[0].Id;
                     $("#myModal").ajaxForm({
                         url: deleteLink + id,
+                        leaveMessage: options.leaveMessage,
                         dataChanged: function () {
                             connect.tableTools.fnSelectNone(); //Fix tabletool bug to disable buttons when no row is selected
                             connect.datatable.fnDraw(false); //redraw the table to show the new data
                         }
                     });
                 }
-
             }
         }
 
@@ -156,43 +158,52 @@ jQuery.fn.extend({//todo ask ays question on close button, implement esc key
     ajaxForm: function (options) {
         var modal = this;
         var $form = null;
+        var bindCloseClass = function (data) {
+            $(modal).find(".modal-close").off("click.close");
+            if (data)
+                $(modal).html(data); //load the new form
+            $(modal).find(".modal-close").on("click.close", function (e) {
+                if ($form.hasClass("dirty")) {
+                    if (!confirm(options.cancelMessage || options.leaveMessage)) return;
+                }
+                $form.trigger("reinitialize.areYouSure");
+                $(modal).closeModal();
+            });
+        }
         var initModelForm = function () {
             $form.areYouSure({
                 change: function () {
                     var form = this;
                     // Enable save button only if the form is dirty. i.e. something to save.
-                    if ($(form).hasClass('dirty')) {
-                        $(form).find('[type="submit"]').removeClass('disabled').removeAttr('disabled');;
+                    if ($(form).hasClass("dirty")) {
+                        $(form).find("[type='submit']").removeClass("disabled").removeAttr("disabled");;
                     } else {
-                        $(form).find('[type="submit"]').addClass('disabled').attr('disabled', 'disabled');;
+                        $(form).find("[type='submit']").addClass("disabled").attr("disabled", 'disabled');;
                     }
                 },
-                'message': 'De wijzigingen zijn niet opgeslagen!'
+                message: options.leaveMessage
             });
+            bindCloseClass();
         };
         $(modal).load(options.url, function (response, textStatus, xhr) {
-            if (textStatus === 'error')
+            if (textStatus === "error")
                 alert(xhr.status + " " + xhr.statusText);
             else {
                 $.validator.unobtrusive.parse(modal);
                 $(modal).openModal({
                     dismissible: false
                 });
-                $form = $(modal).find('form');
+                $form = $(modal).find("form");
                 initModelForm();
 
                 $form.submit(function (ev) {
                     $.ajax({
-                        type: $form.attr('method'),
-                        url: $form.attr('action'),
+                        type: $form.attr("method"),
+                        url: $form.attr("action"),
                         data: $form.serialize(),
                         success: function (data) {
                             if (data) {
-                                $(modal).find('.modal-close').off('click.close');
-                                $(modal).html(data);
-                                $(modal).find(".modal-close").on('click.close', function (e) {
-                                    $(modal).closeModal();
-                                });
+                                bindCloseClass(data);
                             } else {
                                 $(modal).closeModal();
                                 if (typeof (options.dataChanged) === "function")
