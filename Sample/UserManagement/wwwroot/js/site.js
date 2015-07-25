60e55c05ef3fd0ed602436ffb401efd41b460813
ThomKiesewetterPrive <thom@kiesewetter.nl>
2015-07-25 12:25:03 +0200

    Code convention quote

--- Sample/UserManagement/wwwroot/js/site.js
+++ Sample/UserManagement/wwwroot/js/site.js
@@ -2,22 +2,22 @@
 //This script is the same for each jquery-datatable.
 //Define your own functions to extend the column rendering or custom error/succes message
 
-var mvc = { "JQuery": { "Datatables": { ajax: {}, tableTools: {}, column: {} } } };
+var mvc = { 'JQuery': { 'Datatables': { ajax: {}, tableTools: {}, column: {} } } };
 
 mvc.JQuery.Datatables.column.createMailToLink = function (data, type, full, meta) {
-    return "<a href=mailto:" + data + ">" + data + "</a>";
+    return '<a href=mailto:' + data + '>' + data + '</a>';
 }
 mvc.JQuery.Datatables.column.formatDate = function (data, type, full, meta) {
-    return data ? window.moment(data).format(meta.settings.aoColumns[meta.col].mvc6Par || "YY-DD-mm hh:mm:ss") : null;
+    return data ? window.moment(data).format(meta.settings.aoColumns[meta.col].mvc6Par || 'YY-DD-mm hh:mm:ss') : null;
 }
 mvc.JQuery.Datatables.returnNull = function (data) { return null; }
-mvc.JQuery.Datatables.getLengthMenu = function () { return [[5, 50, 100, -1], [5, 50, 100, "All"]] };
+mvc.JQuery.Datatables.getLengthMenu = function () { return [[5, 50, 100, -1], [5, 50, 100, 'All']] };
 mvc.JQuery.Datatables.getLanguage = function () { return {} };
 mvc.JQuery.Datatables.ajax.success = function (data, textStatus, jqXHR) { };
 mvc.JQuery.Datatables.ajax.error = function (jqXHR, textStatus, errorThrown) { alert(errorThrown) };
 mvc.JQuery.Datatables.ajax.load = function (response, textStatus, xhr) {
-    if (textStatus === "error")
-        alert(xhr.status + " " + xhr.statusText);
+    if (textStatus === 'error')
+        alert(xhr.status + ' ' + xhr.statusText);
 
 };
 
@@ -25,7 +25,7 @@ mvc.JQuery.Datatables.ajax.load = function (response, textStatus, xhr) {
 // Read all information that can be used for data request from the table and th tags
 $(document).ready(function () {
     $('table.datatables').each(function () {
-        $(this).MvcDatatable({ leaveMessage: "De wijzigingen zijn niet opgeslagen! toch door gaan?" });
+        $(this).MvcDatatable({ leaveMessage: 'De wijzigingen zijn niet opgeslagen! toch door gaan?' });
         //connect.datatable = give tabletool access to datatable to redraw the table after save form
     });
 });
@@ -34,16 +34,16 @@ jQuery.fn.extend({
     MvcDatatable: function (options) {
         var connect = {};
         var $datatable = $(this);
-        var createLink = $datatable.attr("data-create");
-        var editLink = $datatable.attr("data-edit");
-        var deleteLink = $datatable.attr("data-delete");
+        var createLink = $datatable.attr('data-create');
+        var editLink = $datatable.attr('data-edit');
+        var deleteLink = $datatable.attr('data-delete');
         var getRenderInfo = function () {
             var columns = [];
-            $($datatable.find("thead > tr > th[data-data]")).each(function () {
+            $($datatable.find('thead > tr > th[data-data]')).each(function () {
                 var column = {};
-                var render = $(this).attr("data-render");
+                var render = $(this).attr('data-render');
                 if (render)
-                    if (render.substring(0, 1) === "[")
+                    if (render.substring(0, 1) === '[')
                         column.render = render;
                     else
                         column.render = (mvc.JQuery.Datatables.column[render]);
@@ -54,33 +54,33 @@ jQuery.fn.extend({
         var loadAjaxTabledata = function (sSource, aoData, fnCallback) {
             for (var dataTableRequest = {}, i = 0 ; i < aoData.length; i++) dataTableRequest[aoData[i].name] = aoData[i].value;
             $.ajax({
-                url: $(this).attr("data-url"),
-                type: "POST",
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
+                url: $(this).attr('data-url'),
+                type: 'POST',
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
                 data: JSON.stringify(dataTableRequest),
                 success: function (data, textStatus, jqXHR) {
                     if (connect.tableTools)//Fix tabletool bug to disable buttons when no row is selected
                         connect.tableTools.fnSelectNone();
-                    (mvc.JQuery.Datatables.ajax[$(this).attr("data-succes") || "success"])(data, textStatus, jqXHR);
+                    (mvc.JQuery.Datatables.ajax[$(this).attr('data-succes') || 'success'])(data, textStatus, jqXHR);
                     fnCallback(data, textStatus, jqXHR);
                 },
                 error: function (jqXHR, textStatus, errorThrown) {
-                    (mvc.JQuery.Datatables.ajax[$(this).attr("data-error") || "error"])(jqXHR, textStatus, errorThrown);
+                    (mvc.JQuery.Datatables.ajax[$(this).attr('data-error') || 'error'])(jqXHR, textStatus, errorThrown);
                 }
             });
         };
         var editButton = {
-            sExtends: "select_single",
-            sButtonClass: "waves-effect waves-light disabled modal-trigger",
-            sButtonText: "Edit",
+            sExtends: 'select_single',
+            sButtonClass: 'waves-effect waves-light disabled modal-trigger',
+            sButtonText: 'Edit',
             fnInit: function () {
                 connect.tableTools = this;
             },
             fnClick: function (nButton, oConfig) {
                 if (this.fnGetSelected().length === 1) {
                     var id = this.fnGetSelectedData()[0].Id;
-                    $("#myModal").ajaxForm({
+                    $('#myModal').ajaxForm({
                         url: editLink + id,
                         leaveMessage: options.leaveMessage,
                         dataChanged: function () {
@@ -92,14 +92,14 @@ jQuery.fn.extend({
             }
         }
         var createButton = {
-            sExtends: "text",
-            sButtonClass: "waves-effect waves-light modal-trigger",
-            sButtonText: "Create",
+            sExtends: 'text',
+            sButtonClass: 'waves-effect waves-light modal-trigger',
+            sButtonText: 'Create',
             fnInit: function () {
                 connect.tableTools = this;
             },
             fnClick: function (nButton, oConfig) {
-                $("#myModal").ajaxForm({
+                $('#myModal').ajaxForm({
                     url: createLink,
                     leaveMessage: options.leaveMessage,
                     dataChanged: function () {
@@ -110,16 +110,16 @@ jQuery.fn.extend({
             }
         }
         var deleteButton = {
-            sExtends: "select_single",
-            sButtonClass: "waves-effect waves-light disabled modal-trigger",
-            sButtonText: "Delete",
+            sExtends: 'select_single',
+            sButtonClass: 'waves-effect waves-light disabled modal-trigger',
+            sButtonText: 'Delete',
             fnInit: function () {
                 connect.tableTools = this;
             },
             fnClick: function (nButton, oConfig) {
                 if (this.fnGetSelected().length >= 1) {
                     var id = this.fnGetSelectedData()[0].Id;
-                    $("#myModal").ajaxForm({
+                    $('#myModal').ajaxForm({
                         url: deleteLink + id,
                         leaveMessage: options.leaveMessage,
                         dataChanged: function () {
@@ -139,7 +139,7 @@ jQuery.fn.extend({
         if (deleteLink)
             buttons.push(deleteButton);
         var ttOptions = {
-            sRowSelect: "os",
+            sRowSelect: 'os',
             aButtons: buttons
         }
         connect.datatable = $(this).dataTable({
@@ -147,8 +147,8 @@ jQuery.fn.extend({
             serverSide: true,
             fnServerData: loadAjaxTabledata,
             columns: getRenderInfo(),
-            language: (mvc.JQuery.Datatables[$(this).attr("data-language") || "returnNull"])(),
-            lengthMenu: (mvc.JQuery.Datatables[$(this).attr("data-lengthMenu") || "returnNull"])(),
+            language: (mvc.JQuery.Datatables[$(this).attr('data-language') || 'returnNull'])(),
+            lengthMenu: (mvc.JQuery.Datatables[$(this).attr('data-lengthMenu') || 'returnNull'])(),
             tableTools: ttOptions
         });
     }
@@ -159,14 +159,14 @@ jQuery.fn.extend({//todo ask ays question on close button, implement esc key
         var modal = this;
         var $form = null;
         var bindCloseClass = function (data) {
-            $(modal).find(".modal-close").off("click.close");
+            $(modal).find('.modal-close').off('click.close');
             if (data)
                 $(modal).html(data); //load the new form
-            $(modal).find(".modal-close").on("click.close", function (e) {
-                if ($form.hasClass("dirty")) {
+            $(modal).find('.modal-close').on('click.close', function (e) {
+                if ($form.hasClass('dirty')) {
                     if (!confirm(options.cancelMessage || options.leaveMessage)) return;
                 }
-                $form.trigger("reinitialize.areYouSure");
+                $form.trigger('reinitialize.areYouSure');
                 $(modal).closeModal();
             });
         }
@@ -175,10 +175,10 @@ jQuery.fn.extend({//todo ask ays question on close button, implement esc key
                 change: function () {
                     var form = this;
                     // Enable save button only if the form is dirty. i.e. something to save.
-                    if ($(form).hasClass("dirty")) {
-                        $(form).find("[type='submit']").removeClass("disabled").removeAttr("disabled");;
+                    if ($(form).hasClass('dirty')) {
+                        $(form).find('[type="submit"]').removeClass('disabled').removeAttr('disabled');;
                     } else {
-                        $(form).find("[type='submit']").addClass("disabled").attr("disabled", 'disabled');;
+                        $(form).find('[type="submit"]').addClass('disabled').attr('disabled', 'disabled');;
                     }
                 },
                 message: options.leaveMessage
@@ -186,27 +186,27 @@ jQuery.fn.extend({//todo ask ays question on close button, implement esc key
             bindCloseClass();
         };
         $(modal).load(options.url, function (response, textStatus, xhr) {
-            if (textStatus === "error")
-                alert(xhr.status + " " + xhr.statusText);
+            if (textStatus === 'error')
+                alert(xhr.status + ' ' + xhr.statusText);
             else {
                 $.validator.unobtrusive.parse(modal);
                 $(modal).openModal({
                     dismissible: false
                 });
-                $form = $(modal).find("form");
+                $form = $(modal).find('form');
                 initModelForm();
 
                 $form.submit(function (ev) {
                     $.ajax({
-                        type: $form.attr("method"),
-                        url: $form.attr("action"),
+                        type: $form.attr('method'),
+                        url: $form.attr('action'),
                         data: $form.serialize(),
                         success: function (data) {
                             if (data) {
                                 bindCloseClass(data);
                             } else {
                                 $(modal).closeModal();
-                                if (typeof (options.dataChanged) === "function")
+                                if (typeof (options.dataChanged) === 'function')
                                     options.dataChanged();
                             }
                         },
