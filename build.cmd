12e8801f1266c19a169f8aaaa9dcee27c507eeab
Thom Kiesewetter <thom@kiesewetter.nl>
2015-08-29 22:11:34 +0200

    test appvoyer build

--- build.cmd
+++ build.cmd
@@ -1,7 +1,8 @@
+
 @echo on
 cd %~dp0
-SET BUILDCMD_RELEASE=beta6
 SETLOCAL
+SET BUILDCMD_RELEASE=beta6
 SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
 
 IF EXIST %CACHED_NUGET% goto copynuget
@@ -14,15 +15,19 @@ IF EXIST .nuget\nuget.exe goto restore
 md .nuget
 copy %CACHED_NUGET% .nuget\nuget.exe > nul
 
+.nuget\NuGet.exe sources add -Name aspnetrelease -Source https://www.myget.org/F/aspnetrelease/api/v2
+rem .nuget\NuGet.exe sources 
+rem .nuget\NuGet.exe
+
 :restore
 IF EXIST packages\KoreBuild goto dnvm
-SET DNX_FEED=https://www.myget.org/F/aspnetrelease/api/v2
 IF DEFINED BUILDCMD_RELEASE (
-	.nuget\NuGet.exe install KoreBuild -version 0.2.1-%BUILDCMD_RELEASE%-15006 -ExcludeVersion -o packages -nocache -pre
+	.nuget\NuGet.exe install KoreBuild -version 0.2.1-%BUILDCMD_RELEASE% -ExcludeVersion -o packages -nocache -pre
 ) ELSE (
 	.nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
 )
 .nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
+
 :dnvm
 IF EXIST %USERPROFILE%\.dnx\runtimes\dnx-clr-win-x86.1.0.0-%BUILDCMD_RELEASE% goto coreInstall
 IF DEFINED BUILDCMD_RELEASE (
@@ -30,13 +35,15 @@ IF DEFINED BUILDCMD_RELEASE (
 ) ELSE (
 	CALL packages\KoreBuild\build\dnvm upgrade -runtime CLR -arch x86 
 )
+
 :coreInstall
 IF EXIST %USERPROFILE%\.dnx\runtimes\dnx-CoreCLR-win-x86.1.0.0-%BUILDCMD_RELEASE% goto use
 CALL packages\KoreBuild\build\dnvm install default -runtime CoreCLR -arch x86
 
 :use
-@echo ;%PATH%; | find /C /I "dnx-clr-win-x86.1.0.0-%BUILDCMD_RELEASE%" >nul
+echo ;%PATH%; | find /C /I "dnx-clr-win-x86.1.0.0-%BUILDCMD_RELEASE%" >nul
 if %ERRORLEVEL% EQU 0 goto build
 CALL packages\KoreBuild\build\dnvm use default -runtime CLR -arch x86	
+
 :build
 packages\Sake\tools\Sake.exe -I packages\KoreBuild\build -v -f makefile.shade %*
